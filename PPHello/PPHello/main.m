cf14a8b035a5cd2b4bf8b4eef1db1cb918948532
apple <apple@helloX.local>
2015-08-25 16:32:39 +0800

    hello world

---
+++ PPHello/PPHello/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  PPHello
+//
+//  Created by apple on 15/8/25.
+//  Copyright (c) 2015年 PiPe. All rights reserved.
+/*
+ github first test
+ 
+ by mjokerx
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"helloworld");
+    }
+    return 0;
+}
